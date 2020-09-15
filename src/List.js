b650a1ef02ed80664bb64aaa20897a9a134092ac
tangweikun <819573105@qq.com>
2020-09-15 17:34:37 +0800

    :fire: delete test file

--- src/List.js
+++ src/List.js
@@ -5,6 +5,7 @@ export default function List() {
   const [persons, setPersons] = useState([]);
 
   useEffect(() => {
+    // 这是一个GET请求
     // 在 componentDidMount的时候从接口发起请求获取用户列表
     axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
       const persons = res.data;
