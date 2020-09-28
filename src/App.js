19086872929b7550d0ab852289e465d358b83954
tangweikun <819573105@qq.com>
2020-09-28 08:57:54 +0800

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import './App.css';
 import List from './List';
 import Demo1 from './demo1';
@@ -8,10 +9,31 @@ import Demo3 from './demo3';
 function App() {
   return (
     <div className='App'>
-      <Demo1 />
-      <Demo2 />
-      <Demo3 />
-      <List />
+      <Router>
+        <ul>
+          <li>
+            <Link to='/demo1'>React HTTP POST Requests with Fetch</Link>
+          </li>
+          <li>
+            <Link to='/demo2'>React HTTP GET Requests with Axios</Link>
+          </li>
+          <li>
+            <Link to='/demo3'>TODOLIST</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path='/demo1'>
+            <Demo1 />
+          </Route>
+          <Route path='/demo2'>
+            <Demo2 />
+          </Route>
+          <Route path='/demo3'>
+            <Demo3 />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
