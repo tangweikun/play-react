aa1d84a786feaadb407e7f4d869c0fd3abfcf609
tangweikun <819573105@qq.com>
2020-09-30 17:34:52 +0800

    demo13

---
+++ src/pages/demo13/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class TodoList extends React.Component {
+  constructor() {
+    super();
+    this.state = { todos: [] };
+  }
+
+  addNewTodo() {}
+
+  removeTodo() {}
+
+  setTodoDone() {}
+
+  onAddButtonClick() {}
+
+  render() {
+    return <div>888</div>;
+  }
+}
