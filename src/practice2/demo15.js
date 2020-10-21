e7b31bdb9bcfae14826d004da2a6405b721d3a3b
tangweikun <819573105@qq.com>
2020-10-21 18:55:01 +0800

    add

--- src/practice2/demo15.js
+++ src/practice2/demo15.js
@@ -1,1 +1,11 @@
-// https://www.nowcoder.com/practice/eb86f08c478749868a01861f5ddad28b?tpId=6&&tqId=10992&rp=1&ru=/ta/js-assessment&qru=/ta/js-assessment/question-ranking
+// 难度 ⭐⭐
+// matchesPattern
+// 给定字符串 str，检查其是否符合如下格式
+// 1、XXX-XXX-XXXX
+// 2、其中 X 为 Number 类型
+
+// 答案
+function foo(inputData) {}
+
+// 打印出来的和期望输出一样就说明答案写对了
+console.log(foo('800-555-1212')); // true
---
+++ src/practice2/demo18.js
@@ -0,0 +1,12 @@
+// 难度 ⭐⭐⭐
+// count
+// 实现一个打点计时器，要求
+// 1、从 start 到 end（包含 start 和 end），每隔 100 毫秒 console.log 一个数字，每次数字增幅为 1
+// 2、返回的对象中需要包含一个 cancel 方法，用于停止定时操作
+// 3、第一个数需要立即输出
+
+// 答案
+function foo(inputData) {}
+
+// 打印出来的和期望输出一样就说明答案写对了
+console.log(foo());
