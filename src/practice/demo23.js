7d35e4cee6984377f0419e1f27ffa45beaf5716b
tangweikun <819573105@qq.com>
2020-10-21 13:02:48 +0800

    add

---
+++ src/practice/demo23.js
@@ -0,0 +1,15 @@
+// 难度 ⭐
+// 入参
+const data = {
+  a: [1, 4, 2],
+  b: [3, 9, 7],
+};
+
+// 期望输出
+// const expectOutput = [1, 2, 3, 4, 7, 9]
+
+// 答案
+function foo(inputData) {}
+
+// 打印出来的和期望输出一样就说明答案写对了
+console.log(foo(data));
---
+++ src/practice/demo24.js
@@ -0,0 +1,16 @@
+// 难度 ⭐
+// 入参
+const data = {
+  a: [1, 4, 2],
+  b: [3, 2, 4, 8],
+  c: [2, 3],
+};
+
+// 期望输出
+// const expectOutput = [6, 24, 8, 8]
+
+// 答案
+function foo(inputData) {}
+
+// 打印出来的和期望输出一样就说明答案写对了
+console.log(foo(data));
