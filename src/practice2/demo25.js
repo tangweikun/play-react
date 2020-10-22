87d0d3b50b4ca2253b744263b9fe3be0936102b0
tangweikun <819573105@qq.com>
2020-10-22 13:00:45 +0800

    add

---
+++ src/practice2/demo25.js
@@ -0,0 +1,19 @@
+// 难度 ⭐⭐
+// 把下列数组对象按多字段排序，name升序，age降序
+
+const data = [
+  { name: '张三', age: 38, num: 123 },
+  { name: '李四', age: 15, num: 678 },
+  { name: '王王', age: 16, num: 256 },
+  { name: '老六', age: 28, num: 256 },
+  { name: '老王', age: 16, num: 789 },
+  { name: '大一', age: 23, num: 678 },
+  { name: '大二', age: 20, num: 123 },
+  { name: '大三', age: 40, num: 989 },
+];
+
+// 答案
+function foo(inputData) {}
+
+// 打印出来的和期望输出一样就说明答案写对了
+console.log(foo());
---
+++ src/practice2/demo29.js
@@ -0,0 +1,8 @@
+// 难度 ⭐⭐
+// 出一个函数求出N的阶乘
+
+// 答案
+function foo(inputData) {}
+
+// 打印出来的和期望输出一样就说明答案写对了
+console.log(foo());
